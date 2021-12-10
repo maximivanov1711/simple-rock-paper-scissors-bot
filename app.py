a74359ab2aebe9c07ec23113b93ca0ecd88057cb
maximivanov1711 <maxim.ivanov1711@gmail.com>
2021-12-10 17:10:02 +0300

    Added webhook

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
+import os
+
 from aiogram import executor
 
-from loader import dp
+from loader import dp, bot
 import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
+# webhook settings
+WEBHOOK_URL = "https://simple-rock-paper-scissors-bot.herokuapp.com/"
+
+# webserver settings
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = int(os.environ.get("PORT"))
+
 
 async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL)
     # Устанавливаем дефолтные команды
     await set_default_commands(dispatcher)
 
@@ -14,6 +24,17 @@ async def on_startup(dispatcher):
     await on_startup_notify(dispatcher)
 
 
-if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup)
+async def on_shutdown(dp):
+    await bot.delete_webhook()
 
+
+if __name__ == '__main__':
+    executor.start_webhook(
+        dispatcher=dp,
+        webhook_path="/",
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
